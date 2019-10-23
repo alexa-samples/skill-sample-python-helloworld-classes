29b2b5a481e8cbe647e5f390e349a0042f29e07b
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2019-10-22 23:09:40 -0400

    [ ADD ] added locales to skill via gettext

---
+++ lambda/py/alexa/data.py
@@ -0,0 +1,9 @@
+from gettext import gettext as _
+
+WELCOME_MESSAGE = _(
+    "Welcome, you can say Hello or Help. Which would you like to try?")
+HELLO_MSG = _("Hello Python World from Classes!")
+HELP_MSG = _("You can say hello to me! How can I help?")
+GOODBYE_MSG = _("Goodbye!")
+REFLECTOR_MSG = _("You just triggered {}")
+ERROR = _("Sorry, I had trouble doing what you asked. Please try again.")
